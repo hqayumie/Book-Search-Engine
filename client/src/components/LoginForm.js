150e191d566f0b2d0705707f6d91f0d66f20f8ad
hqayumie <heelaqayumie@gmail.com>
2023-01-26 13:33:22 -0500

    added queries and mutations

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,14 +1,18 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { loginUser } from '../utils/API';
+
+
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+
+  const [login, { error }] = useMutation(USER_LOGIN);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
---
+++ server/schemas/typeDef.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    id: ID!
+    username: String
+    email: String
+    bookCount: Int
+    saveBooks: [Book]
+}
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID
+    user: User
+  }
+  input saveBook {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: bookInput): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
